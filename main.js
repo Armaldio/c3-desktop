ce5dfdb0a6081a4d519d4fb1a9730c3308d7a2c7
Quentin Goinaud <armaldio@gmail.com>
2019-12-23 18:40:45 +0100

    initial release

---
+++ main.js
@@ -0,0 +1,81 @@
+const {app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const { autoUpdater } = require("electron-updater")
+const path = require('path');
+
+let mainWindow;
+
+const isProd = process.env.NODE_ENV === "production";
+
+process.on('uncaughtException', function(err) {
+    //log the message and stack trace
+    fs.writeFileSync('crash.log', err + "\n" + err.stack);
+
+    //do any cleanup like shutting down servers, etc
+
+    //relaunch the app (if you want)
+    app.relaunch({args: []});
+    app.exit(0);
+});
+
+process.on('SIGTERM', function() {
+    fs.writeFileSync('shutdown.log', "Received SIGTERM signal");
+
+    //do any cleanup like shutting down servers, etc
+
+    //relaunch the app (if you want)
+    app.relaunch({args: []});
+    app.exit(0);
+});
+
+// Menu.setApplicationMenu(null);
+
+autoUpdater.checkForUpdatesAndNotify();
+
+async function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            // webSecurity: false,
+            // nodeIntegration: true,
+            // nodeIntegrationInWorker: true,
+            // nodeIntegrationInSubFrames: true,
+            nativeWindowOpen: true,
+            // sandbox: false
+        },
+    });
+
+
+    mainWindow.loadURL('https://editor.construct.net', {userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'});
+    if (!isProd) {
+        mainWindow.removeMenu();
+        mainWindow.webContents.openDevTools();
+    }
+    mainWindow.maximize();
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', async function () {
+    if (mainWindow === null) {
+        await createWindow();
+    }
+});
